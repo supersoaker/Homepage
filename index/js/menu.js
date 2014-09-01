41aa401508ea87b0e4ea2907992cbdbffbe1f40b
supersoaker <visionsoap@yahoo.de>
2014-09-01 17:55:06 +0200

    edited menu

---
+++ index/js/menu.js
@@ -0,0 +1,106 @@
+var wrapper = {};
+var menu = {
+
+	_bindEvents: function( )  {
+		console.log(123)
+		this._hammertime = new Hammer( wrapper );
+		this._activateHammer();
+	},
+
+	_activateHammer: function(){
+		this._hammertime.on('touch dragleft press pan pinch tap dragright swipeleft swiperight release', this._handleEvent);
+	},
+
+	_deactivateHammer: function(){
+		this._hammertime.off('touch dragleft press pan pinch tap dragright swipeleft swiperight release', this._handleEvent);
+	},
+	_handleEvent: function(event) {
+		console.log((event.type))
+		switch (event.type) {
+
+			case 'touch':
+//				if (this._logic.isClosed()) {
+//					if (!this._isInsideSwipeTargetArea(event)) {
+//						event.gesture.stopDetect();
+//					}
+//				}
+
+				break;
+
+			case 'pan':
+//				event.gesture.preventDefault();
+//				console.log(event)
+				var deltaX = event.deltaX;
+				$(wrapper).css('transform', ' translateX(' + deltaX +'px)');
+//				var deltaDistance = this._isRightMenu ? -deltaX : deltaX;
+
+//				var startEvent = event.gesture.startEvent;
+//
+//				if (!('isOpened' in startEvent)) {
+//					startEvent.isOpened = this._logic.isOpened();
+//				}
+//
+//				if (deltaDistance < 0 && this._logic.isClosed()) {
+//					break;
+//				}
+//
+//				if (deltaDistance > 0 && this._logic.isOpened()) {
+//					break;
+//				}
+
+//				var distance = startEvent.isOpened ?
+//					deltaDistance + this._logic.getMaxDistance() : deltaDistance;
+//
+//				this._logic.translate(distance);
+
+				break;
+
+			case 'swipeleft':
+//				event.gesture.preventDefault();
+//
+//				if (this._isRightMenu) {
+//					this.open();
+//				} else {
+//					this.close();
+//				}
+//
+//				event.gesture.stopDetect();
+//				break;
+
+			case 'swiperight':
+//				event.gesture.preventDefault();
+//
+//				if (this._isRightMenu) {
+//					this.close();
+//				} else {
+//					this.open();
+//				}
+//
+//				event.gesture.stopDetect();
+//				break;
+
+			case 'release':
+//				this._lastDistance = null;
+//
+//				if (this._logic.shouldOpen()) {
+//					this.open();
+//				} else if (this._logic.shouldClose()) {
+//					this.close();
+//				}
+//
+//				break;
+		}
+	}
+}
+$(function() {
+	wrapper = document.getElementById('content-wrapper');
+	menu._bindEvents(  );
+//	wrapper.addEventListener('touchmove', function(event) {
+//		var touch = event.targetTouches[0];
+//		console.log(touch.pageX)
+//		// Place element where the finger is
+//		event.target.style.left = touch.pageX + 'px';
+////		event.target.style.top = touch.pageY + 'px';
+//		event.preventDefault();
+//	}, false);
+})
